63f3b6e5218de7c6e280fe1128c2fb909460bb9c
muhammadnuman <141551206+muhammadnuman3146@users.noreply.github.com>
2024-03-17 07:26:37 -0700

    sxn

---
+++ app.js
@@ -0,0 +1,65 @@
+function names(){
+    var names= "Malik Numan"
+    alert(names)
+}
+function number(){
+    var number="12345678"
+    alert(number)
+}
+function subtract(){
+    var statement = document.getElementById("Statement")
+    var values = 6-3
+    var c = values
+    c.value =statement
+
+
+
+  
+   
+    
+}
+function alertName() {
+    alert("Your Name");
+}
+
+function alertNumber() {
+    alert("Your Number");
+}
+
+function showVariableNames() {
+    alert("Variable Names");
+}
+
+function showCamelCaseExample() {
+    alert("CamelCase Example");
+}
+
+function subtractTwoNumbers() {
+    
+}
+
+function multiplyTwoNumbers() {
+    alert("Multiplying Two Numbers");
+}
+
+function sumTwoNumbers() {
+    alert("Summing Two Numbers");
+}
+
+function divideNumber() {
+    alert("Dividing Number");
+}
+
+function calculateSomeNumber() {
+    alert("Calculating Some Number");
+}
+
+function clearStatement() {
+    document.getElementById("Statement").value = "";
+}
+
+function clearResults() {
+    document.getElementById("result").value = "";
+}
+
+
